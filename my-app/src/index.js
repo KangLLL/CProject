1989a9fd43197927548c5bd0d2a54638570a589a
KangLLL <173616365@qq.com>
2018-10-07 20:38:57 -0700

    add turn

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -15,13 +15,14 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true
     }
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
     squares[i] = 'X';
-    this.setState({squares: squares});
+    this.setState({squares: squares, xIsNext: !this.state.xIsNext});
   }
 
 
@@ -31,7 +32,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
