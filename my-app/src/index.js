681e7028bbb32762e345fca35e4b76451088ea0f
KangLLL <173616365@qq.com>
2018-10-08 20:40:46 -0700

    fix

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -21,16 +21,6 @@ class Board extends React.Component {
     }
   }
 
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({squares: squares, xIsNext: !this.state.xIsNext});
-  }
-
-
   renderSquare(i) {
     return <Square value={this.state.squares[i]} 
     onClick={() => this.handleClick(i)}/>;
@@ -61,6 +51,22 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history:history.concat([{
+        squares: squares,
+      }]),
+      xIsNext: !this.state.xIsNext
+    })
+  }
+
   render() {
     const history = this.state.history;
     const current = history[history.length - 1];
@@ -80,7 +86,7 @@ class Game extends React.Component {
           onClick={(i)=>this.handleClick(i)}/>
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
