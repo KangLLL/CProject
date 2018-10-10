d2a7a2fa53176550d90e176793002f57c46aba4d
KangLLL <173616365@qq.com>
2018-10-09 22:05:04 -0700

    fix bug

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -84,7 +84,7 @@ class Game extends React.Component {
       const desc = move ? 'Go to move #' + move : 'Go to game start';
       return (
         <li>
-          <button onclick={()=>this.jumpTo(move)}>{desc}</button>
+          <button onClick={()=>this.jumpTo(move)}>{desc}</button>
         </li>
       );
     });
