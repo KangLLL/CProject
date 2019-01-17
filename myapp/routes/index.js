59c1f6275baf3b54e10777bc43428e1a4d67d06a
KangLLL <173616365@qq.com>
2019-01-17 15:14:29 -0800

    get iphone price

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,30 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-const rp = require('request-promise');
-const $ = require('cheerio');
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  // const urls = [];
-
-  // rp('https://www.apple.com/iphone')
-  //   .then((html)=>{
-  //     var links = $('.chapternav-item > .chapternav-link', html);
-  //     for (let i = 0; i < links.length; i++) {
-  //       urls.push(links[i].attribs.href);
-  //     }
-  //     console.log(urls);
-
-  //     // console.log(html);
-  //   })
-  //   .catch((err)=>{
-  //     console.log(err);
-  //   });
-
-
   res.render('index', { title: 'Price Comparison System' });
 });
 
--- myapp/routes/search.js
+++ myapp/routes/search.js
@@ -1,14 +1,63 @@
 var express = require('express');
 var router = express.Router();
 
+const rp = require('request-promise');
+const $ = require('cheerio');
+
+const fs = require('fs');
+
 /* Product Search */
 router.get('/', function(req, res, next) {
+  // var html = '<ul id="fruits"><li class="apple">Apple</li><li class="orange">Orange</li><li class="pear">Pear</li></ul>';
+
+  // console.log($('li', html).length);
+  
+  // $('li', html).each((i, ele) => {
+  //   console.log(ele.text());
+  // });
   res.render('search', { title: 'Search Item' });
 });
 
 router.post('/', function(req, res, next) {
-  res.render('result', { info:req.body.name });
-  console.log(req.body.name);
+  var usPrices = {};
+  var chinaPrices = [];
+
+  rp('https://www.apple.com/shop/buy-iphone/iphone-7')
+    .then((html)=>{
+  
+      $('.details', html).each((i, ele)=> {
+        console.log($(ele).parent());
+        var carrier = $('.carrier-logos', $(ele)).text();
+        var model = $('.dimensionCapacity', $(ele)).text();
+        var price = $('.current_price', $(ele)).text();
+
+        if (carrier == 'Unlocked') {
+          var infos = $(ele).parent().attr('href').split('/');var size = infos[infos.length - 1].split('-')[0];
+          if (!(size in usPrices)) {
+            usPrices[size] = {};
+          }
+          if (!(model in usPrices[size])) {
+            usPrices[size][model] = price;
+          }
+        }
+      });
+
+      console.log(usPrices);
+      
+      // console.log(caps.length);
+      // for (let i = 0; i < caps.length; i++) {
+      //   console.log(caps[i]);
+      //   console.log(caps[i].textContent());
+      //   models.push(caps[i].text);
+      //   // urls.push(caps[i].attribs.href);
+      // }
+      console.log(models);
+      res.render('result', { info:req.body.models[0] });
+      // console.log(html);
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
 });
 
 module.exports = router;
