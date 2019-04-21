a8ee63dfbecc37f16c3b12cbf27cb536f7793e9a
KangLLL <173616365@qq.com>
2019-04-20 22:55:04 -0700

    add login pages

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Price Comparison System' });
+router.get('/', function (req, res, next) {
+  // res.render('index', { title: 'Price Comparison System' });
+  res.render('login', { title: 'sign up' });
 });
 
 module.exports = router;
