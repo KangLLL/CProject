8346c9ea58364bc97bfdcfdc9e10d7cffd579717
KangLLL <173616365@qq.com>
2019-04-22 09:15:21 -0700

    add validator

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+const user = require('../modules/user');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/signup', function (req, res, next) {
+  res.render('signup', { title: 'Sign Up' });
+});
+
+router.post('/signup', function (req, res, next) {
+  user.signUp(req.body.email, req.body.password, (err, result) => {
+
+  });
+});
+
+router.get('/login', funtion(req, res, next) {
+
+});
+
+router.post('/login', function (req, res, next)) {
+
 });
 
 module.exports = router;
