d7f20518243dfa1f1cfef0e423bb5dadb2ae7e1d
KangLLL <173616365@qq.com>
2019-04-23 15:38:47 -0700

    modify layout

--- myapp/app.js
+++ myapp/app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var session = require('express-session');
+
 var index = require('./routes/index');
 var search = require('./routes/search');
 var tax = require('./routes/tax');
@@ -29,6 +31,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  name: '_price_compare',
+  secret: 'us to chn',
+  resave: false,
+  saveUninitialized: false
+}));
+
 app.use('/', index);
 app.use('/search', search);
 app.use('/tax', tax);
@@ -40,14 +49,14 @@ app.use('/recommend', recommend);
 models.init();
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
