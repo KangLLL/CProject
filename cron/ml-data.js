d8d51537f59d58b8c0b6bf98287cede316948a82
KangLLL <173616365@qq.com>
2019-04-27 20:09:06 -0700

    generate csv file

--- cron/ml-data.js
+++ cron/ml-data.js
@@ -1,10 +1,41 @@
 const models = require('./models');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+const csv = require('csv-parser');
+const fs = require('fs');
+
+const csvWriter = createCsvWriter({
+  path: 'out.csv',
+  encoding: 'utf8',
+  header: [
+    { id: 'NAME', title: 'name' },
+    { id: 'CATEGORY', title: 'category' }
+  ]
+});
 
 models.init((err) => {
   if (!err) {
-    models.Product.loadNameAndCategory((err, results)=>{
+    models.Product.loadNameAndCategory((err, results) => {
       if (!err) {
-        console.log(results);
+        // results.forEach(result => {
+        //   console.log(result.NAME);
+        // });
+        csvWriter.writeRecords(results)
+          .then(() => {
+            console.log('done');
+
+            var results = [];
+            fs.createReadStream('out.csv')
+              .pipe(csv())
+              .on('data', (data) => results.push(data))
+              .on('end', () => {
+                console.log(results);
+
+                results.forEach(result => {
+                  console.log(result.name);
+                });
+              });
+          });
       }
     });
   }
