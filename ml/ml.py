e7c9961c137b24c9ebcc86ecd9c16d928fd7d589
KangLLL <173616365@qq.com>
2019-04-30 11:20:21 -0700

    get data from database directly

--- ml/ml.py
+++ ml/ml.py
@@ -1,38 +1,58 @@
 from __future__ import division
+from mysql.connector import MySQLConnection, Error
+from db_config import connection_dict
+
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import KFold
 from sklearn import svm
 
+data = None
 
-data = pd.read_csv('../cron/out.csv')
+try:
+    conn = MySQLConnection(**connection_dict)
+    cursor = conn.cursor()
+    cursor.execute("select NAME, CATEGORY from usproduct where CATEGORY in (select CATEGORY from usproduct where CATEGORY is not null group by(CATEGORY) having count(*)>100)")
+    rows = cursor.fetchall()
 
-vectorizer = TfidfVectorizer()
-X = vectorizer.fit_transform(data['name'])
-Y = data['category']
+    data = pd.DataFrame(rows, columns=['name', 'category'])
 
-print(Y[0])
+except Error as e:
+    print(e)
 
-kf = KFold(10, True, 2)
-clf = svm.SVC(gamma='scale')
+finally:
+    cursor.close()
+    conn.close()
 
-lin_clf = svm.LinearSVC(C=1)
+# data = pd.read_csv('../cron/out.csv')
+#
+if data is not None:
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(data['name'])
+    Y = data['category']
 
-for train_index, test_index in kf.split(X):
-    clf.fit(X[train_index], Y[train_index])
-    lin_clf.fit(X[train_index], Y[train_index])
-    i = 0
-    correct = 0
-    total = len(test_index)
-    # for y in clf.predict(X[test_index]):
-    for y in lin_clf.predict(X[test_index]):
-        # print('=====')
-        # print(y)
-        # print(Y[test_index].values[i])
-        if y == Y[test_index].values[i]:
-            i += 1
-            correct += 1
-    print(correct / total)
+    print(Y[0])
+
+    kf = KFold(10, False, 2)
+    clf = svm.SVC(gamma='scale')
+
+    lin_clf = svm.LinearSVC(C=1)
+
+    for train_index, test_index in kf.split(X):
+        clf.fit(X[train_index], Y[train_index])
+        lin_clf.fit(X[train_index], Y[train_index])
+        i = 0
+        correct = 0
+        total = len(test_index)
+        for y in clf.predict(X[test_index]):
+        # for y in lin_clf.predict(X[test_index]):
+            # print('=====')
+            # print(y)
+            # print(Y[test_index].values[i])
+            if y == Y[test_index].values[i]:
+                i += 1
+                correct += 1
+        print(correct / total)
 
 
 # print(vectorizer.get_feature_names())
