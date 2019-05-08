ae24243b6371076caeb5f7c6a997377f1e975c86
KangLLL <173616365@qq.com>
2019-05-07 21:44:41 -0700

    show interest

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,10 +6,29 @@ const config = require('../config/config');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  models.Promotion.loadLatestPromotions(config.promotionCount, (err, result) => {
-    if (err) return next(err);
-    res.render('index', { title: 'Price Comparison System', promotions: result });
-  });
+  var load = (interests) => {
+    models.Promotion.loadLatestPromotions(config.promotionCount, (err, result) => {
+      if (err) return next(err);
+      res.render('index', { title: 'Price Comparison System', promotions: result, interests: interests });
+    });
+  }
+  if (req.session.user) {
+    models.Interest.loadInterestCategory(req.session.user, (err, result) => {
+      if (err) return next(err);
+      if (result.length > 0) {
+        models.Promotion.loadPromotionsByCategory(result.map(r => r.CATEGORY), config.promotionCount, (err, result) => {
+          if (err) return next(err);
+          load(result);
+        });
+      }
+      else {
+        load(null);
+      }
+    });
+  }
+  else {
+    load(null);
+  }
 });
 
 module.exports = router;
