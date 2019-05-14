9c390a1625a879440248ffd351361e27e8f73a42
KangLLL <173616365@qq.com>
2019-05-14 13:28:32 -0700

    exit

--- cron/promotion.js
+++ cron/promotion.js
@@ -4,6 +4,14 @@ const axios = require('axios');
 const $ = require('cheerio');
 const dayjs = require('dayjs');
 
+function exit(value) {
+  process.exit(value);
+  // var stdin = process.openStdin();
+  // stdin.addListener('data', d => {
+  //   process.exit(value);
+  // });
+}
+
 axios.get('https://camelcamelcamel.com/')
   .then(res => {
     var html = res.data;
@@ -17,17 +25,17 @@ axios.get('https://camelcamelcamel.com/')
     });
     models.init((err) => {
       if (!err) {
-        models.Product.insertOrUpdatePromotion(result, dayjs().format('YYYY-MM-DD'), (e, r) => { 
+        models.Product.insertOrUpdatePromotion(result, dayjs().format('YYYY-MM-DD'), (e, r) => {
           if (e) {
             console.log(e);
-            process.exit(1);
+            exit(1);
           }
-          process.exit(0);
+          exit(0);
         });
       }
     });
   })
   .catch(err => {
     console.log(err);
-    process.exit(1);
+    exit(1);
   });
